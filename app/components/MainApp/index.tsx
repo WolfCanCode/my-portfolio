47f476d252c8571fcc2ba7845b4c48ba5a136c61
Tommy Le <wolfcancode@gmail.com>
2022-03-15 15:21:55 +0700

    update: prevent mobile

--- app/components/MainApp/index.tsx
+++ app/components/MainApp/index.tsx
@@ -10,19 +10,26 @@ export const MainApp = () => {
   const runningApps = useRecoilValue<number[]>(runningAppsState);
   const openApp = useOpenApp();
   return (
-    <div
-      className={"h-screen w-screen bg-gradient-to-r from-cyan-500 to-blue-500"}
-    >
-      {(runningApps &&
-        runningApps.length &&
-        runningApps.map((id: number) => <Window key={id} id={id} />)) ||
-        null}
-      <TaskBar>
-        <AppIcon id={0} onClick={() => openApp("New Window", 0)} />
-        <AppIcon id={1} onClick={() => openApp("New Window 1", 1)} />
-        <AppIcon id={2} onClick={() => openApp("New Window 2", 2)} />
-        <AppIcon id={3} onClick={() => openApp("New Window 3", 3)} />
-      </TaskBar>
-    </div>
+    <>
+      <div
+        className={
+          "hidden lg:block h-screen w-screen bg-gradient-to-r from-cyan-500 to-blue-500"
+        }
+      >
+        {(runningApps &&
+          runningApps.length &&
+          runningApps.map((id: number) => <Window key={id} id={id} />)) ||
+          null}
+        <TaskBar>
+          <AppIcon id={0} onClick={() => openApp("New Window", 0)} />
+          <AppIcon id={1} onClick={() => openApp("New Window 1", 1)} />
+          <AppIcon id={2} onClick={() => openApp("New Window 2", 2)} />
+          <AppIcon id={3} onClick={() => openApp("New Window 3", 3)} />
+        </TaskBar>
+      </div>
+      <div className={"lg:hidden block text-center text-red-500"}>
+        Sorry but this app doesn't support for mobile
+      </div>
+    </>
   );
 };
