2ab8f178dde347f242462a5763ff127332f847b9
Tommy Le <wolfcancode@gmail.com>
2022-03-15 17:52:00 +0700

    trigger: redeploy

--- app/components/apps/Calculator/index.tsx
+++ app/components/apps/Calculator/index.tsx
@@ -6,7 +6,7 @@ export const Calculator = () => {
       <iframe
         width="219"
         height="302"
-        src="http://calculator-1.com/outdoor/?f=e0e0e0&r=ffffff"
+        src="https://calculator-1.com/outdoor/?f=e0e0e0&r=ffffff"
         scrolling="no"
         frameBorder="0"
       ></iframe>
